857419e911e1b10c8a64961019cefe8651197679
Josh Bryant <kaytal@gmail.com>
2016-01-06 15:04:16 -0600

    Added Tests for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
-  
+  root 'static_pages#home'
+
+  get 'contact' => 'static_pages#contact'
 
   
-  get 'static_pages/home'
+  #get 'static_pages/home'
 
-  get 'static_pages/contact'
+  #get 'static_pages/contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Josh Bryant | Web Developer | Home"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Josh Bryant | Web Developer | Contact"
   end
 
 end
