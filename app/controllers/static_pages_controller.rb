a8a0fcb42acd95cd8e746772b7952ed4ffae1c49
Josh Bryant <kaytal@gmail.com>
2016-01-07 15:47:12 -0600

    Added instagram and twitter feed to home static page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
+  	@instagram = Instagram.user_recent_media("219769686", {:count => 4})
   end
 
   def contact
---
+++ config/initializers/instagram.rb
@@ -0,0 +1,5 @@
+require "instagram"
+	Instagram.configure do |config|
+	config.client_id = "249ceae1bebb4fd8b091194ad0c77a1e"
+	config.access_token = "219769686.249ceae.137e29b3e91a43778ec3e47ffd6fb67a"
+end
