d3fa791357b8663f516d864cea542a06675adce7
Josh Bryant <Kaytal@gmail.com>
2016-01-09 19:13:58 -0600

    Made the title more DRY, changed the tests to accomodate

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Josh Bryant | Web Developer | Home"
+    assert_select "title", "Josh Bryant | Web Developer"
   end
 
   test "should get contact" do
