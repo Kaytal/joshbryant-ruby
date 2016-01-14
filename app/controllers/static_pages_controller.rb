88b75b4e0ca04171c819100d0d40fd2ca4a133de
Josh Bryant <kaytal@gmail.com>
2016-01-14 14:59:28 -0600

    Started with my twitter integration, little styling for the footer and what not

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,19 @@
 class StaticPagesController < ApplicationController
   def home
   	@instagram = Instagram.user_recent_media("219769686", {:count => 4})
+
+  	client = Twitter::REST::Client.new do |config|
+		  config.consumer_key        = "Zst4O4BnnpuU5eUmvFcqFaKnA"
+		  config.consumer_secret     = "2UQvcwDRu9waVCSiVEG1I1Kquwl9DWCrLgfj3sil4PCkZCx7QA"
+		  config.access_token        = "388513379-Tjp1mmifnaSR2GFSypvlyjV0ZfR1ASyeheeB9HcF"
+		  config.access_token_secret = "9NGKpCfXUYGRHaYHD2B6ixWy0kK36p6Azebyt5FdxLZiQ"
+		end
+
+  	@tweets = client.user_timeline("Kaytal1", :count => 5)
+  		respond_to do |format|
+  			format.html # index.html.erb
+  			format.json { render :json => @posts }
+  		end
   end
 
   def contact
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,4 @@
+require 'twitter-text'
 module ApplicationHelper
 	# Returns the full title on a per-page basis.
 	def full_title(page_title = '')
@@ -8,4 +9,12 @@ module ApplicationHelper
 			base_title + " | " + page_title
 		end
 	end
+
+	#Twitter Text helper
+	include Twitter::Autolink
+ 
+  def twitter_text(text)
+    text = auto_link(text)
+    text ? text.html_safe : ''
+  end
 end
