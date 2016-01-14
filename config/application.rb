e101a72be2af5d17eef60b4fcc43c8bb53d22f47
Josh Bryant <kaytal@gmail.com>
2016-01-14 15:11:33 -0600

    Added config to load static assets

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.service_static_assets = true
+
 module JoshbryantMe
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
