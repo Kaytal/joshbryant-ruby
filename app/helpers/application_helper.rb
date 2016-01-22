c59f2caca873a483e105b7d03efb57c8d406b6dc
Josh Bryant <kaytal@gmail.com>
2016-01-22 12:31:43 -0600

    Added bootsy then removed it promptly, working on figuring out the precompiling issue

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 require 'twitter-text'
 module ApplicationHelper
+  class HTMLwithPygments < Redcarpet::Render::HTML
+    def block_code(code, language)
+      Pygments.highlight(code, lexer: language)
+    end
+  end
 	# Returns the full title on a per-page basis.
 	def full_title(page_title = '')
 		base_title = "Josh Bryant | Web Developer"
@@ -16,5 +21,19 @@ module ApplicationHelper
   def twitter_text(text)
     text = auto_link(text)
     text ? text.html_safe : ''
+  end
+
+  def markdown(content)
+  	renderer = HTMLwithPygments.new(hard_wrap: true, filter_html: true)
+  	options = {
+  		autolink: true,
+  		no_intra_emphasis: true,
+  		disable_indented_code_blocks: true,
+  		fenced_code_blocks: true,
+  		lax_html_blocks: true,
+  		strikethrough: true,
+  		superscript: true
+  	}
+  	Redcarpet::Markdown.new(renderer, options).render(content).html_safe
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160120220627) do
+ActiveRecord::Schema.define(version: 20160122174856) do
+
+  create_table "bootsy_images", force: :cascade do |t|
+    t.string   "image_file"
+    t.integer  "image_gallery_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
